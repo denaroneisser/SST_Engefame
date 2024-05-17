fb68e5261bcc48ac0e502aae926f35068867dd07
Neisser Romanelli <116816843+denaroneisser@users.noreply.github.com>
2024-05-17 14:34:15 +0000

    Criado a Pagina Index chamando tables principais.

---
+++ sst/index.php
@@ -0,0 +1,4 @@
+<button class="btn btn-primary" type="submit" href="assets/pages/ListarCategorias.php">Categorias</button>
+<button class="btn btn-primary" type="submit" href="assets/pages/ListarFuncionarios.php">Funcionários</button>
+<button class="btn btn-primary" type="submit">Treinamentos</button>
+<button class="btn btn-primary" type="submit">Historico Treinamentos</button>
