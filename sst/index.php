96c0a6d8d42793c0b19b5a732858d186b4b3551d
Neisser Romanelli <116816843+denaroneisser@users.noreply.github.com>
2024-05-17 14:40:25 +0000

    reparo de erros no index

--- sst/index.php
+++ sst/index.php
@@ -1,4 +1,4 @@
-<button class="btn btn-primary" type="submit" href="assets/pages/ListarCategorias.php">Categorias</button>
-<button class="btn btn-primary" type="submit" href="assets/pages/ListarFuncionarios.php">Funcionários</button>
-<button class="btn btn-primary" type="submit">Treinamentos</button>
-<button class="btn btn-primary" type="submit">Historico Treinamentos</button>
+<a href="assets/pages/ListarCategorias.php">Categorias</a>
+<a href="assets/pages/ListarFuncionarios.php">Funcionários</a>
+<a href="assets/pages/ListarTreinamentos.php">Treinamentos</a>
+<a href="assets/pages/ListarHistoricoTreinamentos.php">Historico Treinamentos</a>
