ed271cc921867e56689538393a2a0631cf7b3017
Neisser Romanelli <116816843+denaroneisser@users.noreply.github.com>
2024-05-19 17:40:43 +0000

    .

--- assets/pages/CadastroHistoricoTreinamento.php
+++ assets/pages/CadastroHistoricoTreinamento.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-<?php
-//IMPORTANDO BIBLIOTECAS/DEPENDENCIAS
-require_once("../objects/Categorias.php");
-require_once("../objects/Funcionarios.php");
-require_once("../objects/Treinamentos.php");
-require_once("../objects/Historico_Treinamentos.php");
-require_once("../objects/Empresas.php");
-?>
-<link rel="stylesheet" href="../styles/CadastroFuncionario.css">
-<script src="../scripts/CadastroHistoricoTreinamento.js"></script>
+    <?php
+    //IMPORTANDO BIBLIOTECAS/DEPENDENCIAS
+    require_once("../objects/Categorias.php");
+    require_once("../objects/Funcionarios.php");
+    require_once("../objects/Treinamentos.php");
+    require_once("../objects/Historico_Treinamentos.php");
+    require_once("../objects/Empresas.php");
+    ?>
+    <link rel="stylesheet" href="../styles/CadastroFuncionario.css">
+    <script src="../scripts/CadastroHistoricoTreinamento.js"></script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro Funcionarios</title>
@@ -21,7 +21,7 @@ require_once("../objects/Empresas.php");
         <form action="CadastroHistoricoTreinamento.php" method="POST">
             <div class="form-group">
                 <label for="idtreinamento">Nome do Treinamento</label>
-                <select id="idtreinamento" name="idtreinamento" required>
+                <select id="idtreinamento" name="idTreinamento" required>
                     <option value="">Selecione</option>
                     <?php
                         // Chame a função getCategorias para obter todas as categorias
@@ -29,7 +29,7 @@ require_once("../objects/Empresas.php");
                         // Verifique se há categorias e exiba-as
                         if ($treinamentos) {
                             foreach ($treinamentos as $treinamento) {
-                                echo "<option value='" . $treinamento["idTreinamento"] . "'>" . $treinamento["Nome"] . "</option>";;
+                                echo "<option value='" . $treinamento["idTreinamento"] . "'>" . $treinamento["Nome"] . "</option>";
                             }
                         } else {
                             echo "Erro ao obter Treinamentos.";
@@ -38,29 +38,27 @@ require_once("../objects/Empresas.php");
                 </select>
             </div>
             <div class="form-group">
-                <label for="idtreinamento">Nome da Empresa Ministrante</label>
-                <select id="idtreinamento" name="idtreinamento" required>
+                <label for="idEmpresa">Nome da Empresa Ministrante</label>
+                <select id="idEmpresa" name="idEmpresa" required>
                     <option value="">Selecione</option>
                     <?php
-                        // Chame a função getCategorias para obter todas as categorias
                         $empresas = GetEmpresasALL();
-                        // Verifique se há categorias e exiba-as
                         if ($empresas) {
                             foreach ($empresas as $empresa) {
-                                echo "<option value='" . $empresa["idEmpresas"] . "'>" . $empresa["Nome"] . "</option>";;
+                                echo "<option value='" . $empresa["idEmpresas"] . "'>" . $empresa["Nome"] . "</option>";
                             }
                         } else {
-                            echo "Erro ao obter Treinamentos.";
+                            echo "Erro ao obter Empresas.";
                         }
                     ?>
                 </select>
             </div>
             <div class="form-group">
-                <label for="nome">Data da Realização do Treinamento</label>
+                <label for="data_realizacao">Data da Realização do Treinamento</label>
                 <input type="date" id="data_realizacao" name="data_realizacao" required value="<?php echo date('Y-m-d'); ?>">
             </div>
             <div class="form-group">
-                <label for="nome">Data da Expiração do Treinamento</label>
+                <label for="data_validade">Data da Expiração do Treinamento</label>
                 <input type="date" id="data_validade" name="data_validade" required>
             </div>
             <div class="form-group">
@@ -71,12 +69,13 @@ require_once("../objects/Empresas.php");
                 <label for="carga_horaria">Carga Horária</label>
                 <input type="time" id="carga_horaria" name="carga_horaria" required>
             </div>
-            <div class="form-group">Valor do Treinamento por Pessoa</label>
-            <label for="curso_pago">É pago?</label>
-                <select id="comprovacao" name="curso_pago" required>
+            <div class="form-group">
+                <label for="curso_pago">É pago?</label>
+                <select id="curso_pago" name="curso_pago" required>
                     <option value="1">SIM</option>
                     <option value="0">Não</option>
                 </select>
+                <label for="preco_unitario">Valor do Treinamento por Pessoa</label>
                 <input type="number" id="preco_unitario" name="preco_unitario" required placeholder="0.00">
             </div>
             <div class="form-group">
@@ -94,34 +93,27 @@ require_once("../objects/Empresas.php");
                 </select>
             </div>
             <div class="form-group">
-                <label for="comprovacao">Funcionarios Participantes</label>
-            <input type="text" id="funcionario" name="funcionario" oninput="buscarFuncionarios(this.value)">
-            <div id="funcionario-lists" class="funcionario-lists"></div>
-            <div id="informacoes-selecionadas" class="informacoes-selecionadas"></div>
+                <label for="funcionario">Funcionarios Participantes</label>
+                <input type="text" id="funcionario" name="funcionario" oninput="buscarFuncionarios(this.value)">
+                <div id="funcionario-lists" class="funcionario-lists"></div>
+                <div id="informacoes-selecionadas" class="informacoes-selecionadas"></div>
             </div>
-            <div cform-group">
-                <button type="submit" >Cadastrar</button>
+            <div class="form-group">
+                <button type="submit">Cadastrar</button>
             </div>
         </form>
     </div>
 
-
-
-
-
     <script>
-        //=============================================================================================
         // Array para armazenar as informações selecionadas
         const informacoesSelecionadas = [];
 
         // Função para buscar funcionários por nome
         function buscarFuncionarios(nome) {
-            // Verifica se o campo de busca está vazio
             if (nome === '') {
                 document.getElementById('funcionario-lists').innerHTML = '';
                 return;
             }
-            // Faz uma requisição AJAX para buscar os funcionários
             const xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
@@ -134,7 +126,6 @@ require_once("../objects/Empresas.php");
 
         // Função para adicionar uma informação selecionada
         function adicionarInformacao(cpf, nome) {
-            // Verifica se a informação já foi adicionada
             if (!informacoesSelecionadas.includes(cpf)) {
                 informacoesSelecionadas.push(cpf);
                 const container = document.getElementById('informacoes-selecionadas');
@@ -162,41 +153,43 @@ require_once("../objects/Empresas.php");
             }
         }
     </script>
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $idTreinamento = $_POST['idTreinamento'];
-    $idEmpresa = $_POST['idEmpresa'];
-    $data_realizacao = $_POST['data_realizacao'];
-    $data_validade = $_POST['data_validade'];
-    $instrutor = $_POST['instrutor'];
-    $carga_horaria = $_POST['carga_horaria'];
-    $curso_pago = $_POST['curso_pago'];
-    $preco_unitario = $_POST['preco_unitario'];
-    $modalidade = $_POST['modalidade'];
-    $comprovacao = $_POST['comprovacao'];
-    $funcionarios = json_decode($_POST['informacoesSelecionadas'], true); // Decodifica os funcionários selecionados
 
-    foreach ($funcionarios as $cpf) {
-        $result = setHistoricoTreinamento(
-            $idTreinamento,
-            $cpf,
-            $instrutor,
-            $data_realizacao,
-            $data_validade,
-            $comprovacao,
-            $modalidade,
-            $carga_horaria,
-            $curso_pago,
-            $preco_unitario,
-            $idEmpresa
-        );
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $idTreinamento = $_POST['idTreinamento'];
+        $idEmpresa = $_POST['idEmpresa'];
+        $data_realizacao = $_POST['data_realizacao'];
+        $data_validade = $_POST['data_validade'];
+        $instrutor = $_POST['instrutor'];
+        $carga_horaria = $_POST['carga_horaria'];
+        $curso_pago = $_POST['curso_pago'];
+        $preco_unitario = $_POST['preco_unitario'];
+        $modalidade = $_POST['modalidade'];
+        $comprovacao = $_POST['comprovacao'];
+        $funcionarios = json_decode($_POST['informacoesSelecionadas'], true); // Decodifica os funcionários selecionados
+
+        foreach ($funcionarios as $cpf) {
+            $result = setHistoricoTreinamento(
+                $idTreinamento,
+                $cpf,
+                $instrutor,
+                $data_realizacao,
+                $data_validade,
+                $comprovacao,
+                $modalidade,
+                $carga_horaria,
+                $curso_pago,
+                $preco_unitario,
+                $idEmpresa
+            );
+        }
+
+        if ($result) {
+            echo("<script>alert('Histórico de Treinamento Cadastrado com Sucesso!');</script>");
+        } else {
+            echo("<script>alert('Erro ao Cadastrar Histórico de Treinamento');</script>");
+        }
     }
-
-    if ($result) {
-        echo("<script>alert('Histórico de Treinamento Cadastrado com Sucesso!');</script>");
-    } else {
-        echo("<script>alert('Erro ao Cadastrar Histórico de Treinamento');</script>");
-    }
-}
-
-?>
+    ?>
+</body>
+</html>
